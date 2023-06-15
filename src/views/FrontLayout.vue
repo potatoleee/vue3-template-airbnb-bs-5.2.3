62058d9fb820d108f67485f98a7bc8497b77f51c
dean <>
2023-06-15 18:03:50 +0800

    first commit

---
+++ src/views/FrontLayout.vue
@@ -0,0 +1,14 @@
+<script setup></script>
+<template>
+  <header>
+    導覽列
+    <RouterLink to="/"> home </RouterLink>
+    <RouterLink to="about"> about </RouterLink>
+    <RouterLink to="login"> login </RouterLink>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+  <footer class="mt-auto">footer</footer>
+</template>
+<style lang="scss"></style>
---
+++ src/views/IndexView.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <h1>首頁</h1>
+  <font-awesome-icon icon="fa-solid fa-house" />
+</template>
+
+<style lang="scss">
+@import '@/assets/style/all.scss';
+</style>
